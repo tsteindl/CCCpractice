c50c63c5aa98a57e91fc39a8dc307bbad3209060
tsteindl <tobias.steindl@gmx.net>
2022-11-10 20:34:28 +0100

    first commit

---
+++ turning_radius.py
@@ -0,0 +1,35 @@
+import math
+
+wheelbase = 2.13
+dist = 4.3
+steeringangle = 23
+
+def degToRad(deg):
+    return deg/180*math.pi
+
+
+def calcTurnRadius(wheelbase, steeringangle):
+    return round(wheelbase / math.sin(degToRad(steeringangle)), 3)
+
+
+def circumference(radius):
+    return 2*radius*math.pi
+
+
+def distToAngle(wheelbase, radius):
+    return math.asin(wheelbase/radius)
+
+
+alpha = (distToAngle(wheelbase, calcTurnRadius(wheelbase, steeringangle)))
+
+r = calcTurnRadius(wheelbase, steeringangle)
+y = r * math.sin(dist/r)
+s = r * math.cos(dist/r)
+x = r - s
+
+
+print(x)
+print(y)
+print(dist/circumference(calcTurnRadius(wheelbase, steeringangle)) * 360)
+
+
